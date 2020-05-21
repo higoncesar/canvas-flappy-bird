ba76b4934f0694ca5af8d3333bf087a2d55cb14b
Higon Cesar <higondourado@denox.com.br>
2020-05-20 21:04:44 -0300

    Initial structure

---
+++ game.js
@@ -0,0 +1,9 @@
+console.log("Flap Bird ");
+
+const sprites = new Image();
+
+sprites.src = "./sprites.png";
+
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
